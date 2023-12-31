e0c8552eddfc8541a7e3f8e41d87120bf5c4f523
Allowwed <surendranathdas24@gmail.com>
2023-12-31 13:41:02 +0530

    k

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     consultBtn.addEventListener("click", redirectToForm);
 });
+
+
+window.onload = () => {
+
+	const toggler = new Toggler();
+
+	document.getElementById("navbtn").addEventListener("click", () => {
+			toggler.toggleSlide("navLink", "right", .5);
+			toggler.toggleClass("open_text", "fa-bars", "fa-xmark");
+	});
+}
