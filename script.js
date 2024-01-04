61922fa348c1395426b27b7b2e08172c3c37d224
Allowwed <surendranathdas24@gmail.com>
2024-01-04 22:30:24 +0530

    Optimization

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 window.onload = () => {
-
 	const toggler = new Toggler();
 
 	document.getElementById("navbtn").addEventListener("click", () => {
@@ -18,3 +17,16 @@ window.onload = () => {
 			toggler.toggleClass("open_text", "fa-bars", "fa-xmark");
 	});
 }
+
+
+// document.addEventListener('click', function (event) {
+//     const navLink = document.getElementById('navLink');
+//     const navbtn = document.getElementById('navbtn');
+//     const open_text = document.getElementById('open_text');
+
+//     // Check if the clicked element is not the #navLink itself
+//     if (event.target !== navLink && event.target !== navbtn && open_text.classList.contains('x-mark')) {
+//         const toggler = new Toggler();
+//         toggler.toggleSlide("navLink", "right", .4);
+//     }
+// });
